f0a53089cc06d7ebdeb5a4094d9657fbc5266708
Macrine <macrinejangu@gmail.com>
2026-08-13 13:38:18 +0300

    Worked on Error Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="web-footer">
-      <p>By joining you agree to the terms of service &amp; privacy guidelines.</p>
+      <p>By joining you agree to the terms of service and; privacy guidelines.</p>
       <p>© 2026 VERIFY.AI. All rights reserved.</p>
     </footer>
   );
